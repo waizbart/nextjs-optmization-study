91bdc3f3b3d94d94dfbf998917ad435f26ca7fef
waizbart <guilhermewaizbart@gmail.com>
2026-01-19 23:49:31 -0300

    feat: add UI components

---
+++ app/ui/index.tsx
@@ -0,0 +1,9 @@
+import SpinningTitle from "@/components/spinning-title";
+
+export default function HomeUI() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <SpinningTitle />
+        </div>
+    );
+}
