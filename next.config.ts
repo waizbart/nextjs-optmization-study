5e22bd4edc0909ec87549a93e5519ab6c684b40b
waizbart <guilhermewaizbart@gmail.com>
2026-01-19 23:49:23 -0300

    config: add bundle analyzer and image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig)
